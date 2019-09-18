00772e43f8ba99d53dc965b4951e4fb336789cef
unknown <mwelch1194@gmail.com>
2019-09-18 10:35:55 -0500

    new

---
+++ TrueCoders/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrueCoders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
